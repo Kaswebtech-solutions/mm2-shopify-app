93e00cb74ae4781c2330c88d469bc190adb21e3e
Kaswebtech-solutions <dev.kaswebtech@gmail.com>
2024-08-02 10:39:27 +0530

    change url & add orderid,email of customer

--- extensions/checkout-ui-extension/src/Checkout.jsx
+++ extensions/checkout-ui-extension/src/Checkout.jsx
@@ -1,21 +1,41 @@
 import {
   reactExtension,
   Button,
-  useSettings
-  // useEmail
+  useSettings,
+  useEmail,
+  useApi
 } from '@shopify/ui-extensions-react/checkout';
 import React from 'react';
 
-const thankYouBlock = reactExtension("purchase.thank-you.block.render", () => <ExtensionRender />);
+const thankYouBlock = reactExtension("purchase.thank-you.block.render", () => <ExtensionThankuRender />);
 export { thankYouBlock };
 
-const orderDetailsBlock = reactExtension("customer-account.order-status.block.render", () => <ExtensionRender />);
+const orderDetailsBlock = reactExtension("customer-account.order-status.block.render", () => <ExtensionOrderRender />);
 export { orderDetailsBlock };
 
-function ExtensionRender() {
-  // const email = useEmail();
+function ExtensionThankuRender() {
+  const data= useApi();
+  const email = useEmail();
+  let orderId = data.orderConfirmation.current.order.id;
+  orderId= orderId.match(/(\d+)$/)[0]; 
   const {url: buttonurl} = useSettings();
-  const url = buttonurl ?? 'https://mm2.land/';
+  const baseUrl = buttonurl ?? 'https://claim.mm2.land/';
+  const url = `${baseUrl}?order=${orderId}&email=${email}`;
+  return (
+    <>
+      <Button to={url} className='custom-button'>CLAIM ORDER</Button>
+    </>
+  );
+}
+
+function ExtensionOrderRender() {
+  const email = useEmail();
+  const data = useApi();
+  let orderId = data.order.current.id;
+  orderId= orderId.match(/(\d+)$/)[0]; 
+  const {url: buttonurl} = useSettings();
+  const baseUrl = buttonurl ?? 'https://claim.mm2.land/';
+  const url = `${baseUrl}?order=${orderId}&email=${email}`;
   return (
     <>
       <Button to={url} className='custom-button'>CLAIM ORDER</Button>
